1670cecbc372fa1a6b5d5cc83fcb0c8de9bd6b19
DirkFi <ming557@outlook.com>
2024-09-08 09:39:29 -0700

    Add Input and Toggle box

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,9 +1,11 @@
 //src/pages/home.rs
+use web_sys::{console, HtmlInputElement};
 use yew::html::Scope;
 use yew::prelude::*;
+
 #[derive(Clone, Properties, PartialEq)]
 struct Course {
-    id: i32,
+    id: usize,
     name: String,
     teacher: String,
     description: String,
@@ -22,11 +24,13 @@ struct State {
 
 pub struct Home {
     state: State,
+    checks: Vec<bool>,
     link: Scope<Self>,
 }
 
 pub enum Msg {
-    Chosen(i32),
+    UpdateValue(usize, String),
+    Chosen(usize),
 }
 
 impl Component for Home {
@@ -34,6 +38,7 @@ impl Component for Home {
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
+        console::log_1(&"Hello from Yew in creation!".into());
         let link = ctx.link().clone();
         let courses = vec![Course {
             id: 0,
@@ -42,27 +47,44 @@ impl Component for Home {
             description: "".to_string(),
             image: "".to_string(),
         }];
-        let grades = vec![];
+        let grades: Vec<Grade> = vec![];
+        let checks: Vec<bool> = vec![];
         Self {
             state: State { courses, grades },
+            checks,
             link,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Chosen(id) => {
-                let course = self.state.courses.iter().find(|c| c.id == id).unwrap();
+            Msg::UpdateValue(index, value) => {
+                console::log_1(&"Hello from update 1st!".into());
+                if let Some(check_box) = self.checks.get(index) {
+                    if *check_box {
+                        let course = self.state.courses.iter().find(|c| c.id == index).unwrap();
+                        let grade = self.state.grades.iter_mut().find(|g| g.course.id == index);
+                        if let Some(cg) = grade {
+                            // update current course grade point to the new one
+                            cg.point = value.parse::<f32>().unwrap();
+                        } else {
+                            self.state.grades.push(Grade {
+                                course: course.clone(),
+                                point: value.parse::<f32>().unwrap(),
+                            });
+                        }
+                        console::log_1(&"Hello from Yew!".into());
+                    }
+                }
+                true
+            }
 
-                let grade = self.state.grades.iter_mut().find(|g| g.course.id == id);
-                if let Some(cg) = grade {
-                    // update current course grade point to the new one
-                    cg.point = 89.0;
+            Msg::Chosen(id) => {
+                console::log_1(&"Hello from update 2nd!".into());
+                if let Some(check_box) = self.checks.get_mut(id) {
+                    *check_box = !*check_box;
                 } else {
-                    self.state.grades.push(Grade {
-                        course: course.clone(),
-                        point: 1.0,
-                    })
+                    self.checks.push(true);
                 }
                 true
             }
@@ -73,23 +95,40 @@ impl Component for Home {
         true
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         let courses: Vec<Html> = self
             .state
             .courses
             .iter()
             .map(|course: &Course| {
-                let course_id = course.id;
-                html! {
-                <div>
-                <img src={course.image.clone()}/>
-
-                <div> {course.name.clone()}</div>
-                <div> {course.teacher.clone()}</div>
-                <div> {course.description.clone()}</div>
-
-                </div>
+                let idx = course.id;
+                let mut point = String::from("0.0");
+                let mut checked = false;
+                if idx < self.state.grades.len() {
+                    point = self.state.grades[idx].point.to_string();
                 }
+                if idx < self.checks.len() {
+                    checked = self.checks[idx];
+                }
+                let oninput = ctx.link().callback(move |e: InputEvent| {
+                    let input: HtmlInputElement = e.target_unchecked_into();
+                    Msg::UpdateValue(idx, input.value())
+                });
+
+                let ontoggle = ctx.link().callback(move |_| Msg::Chosen(idx));
+                html! {
+                     <div>
+                     <img src={course.image.clone()}/>
+
+                     <div> {course.name.clone()}</div>
+                     <div> {course.teacher.clone()}</div>
+                     <div> {course.description.clone()}</div>
+                <div>
+                    <input type="number" step="any"  {oninput} />
+                    <input type="checkbox"  {ontoggle} />
+                </div>
+                </div>
+                     }
             })
             .collect();
         html! { <span>{courses}</span> }
