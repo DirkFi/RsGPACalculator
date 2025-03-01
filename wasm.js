66fc98ae2f44036b76edfcd6730be558b8bc7562
DirkFi <ming557@outlook.com>
2025-02-28 17:13:43 -0800

    Remove redundant button

--- wasm.js
+++ wasm.js
@@ -1111,16 +1111,16 @@ function __wbg_get_imports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper6087 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 638, __wbg_adapter_40);
+    imports.wbg.__wbindgen_closure_wrapper5992 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 632, __wbg_adapter_40);
         return addHeapObject(ret);
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper8006 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 740, __wbg_adapter_43);
+    imports.wbg.__wbindgen_closure_wrapper7911 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 734, __wbg_adapter_43);
         return addHeapObject(ret);
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper8389 = function() { return logError(function (arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 753, __wbg_adapter_46);
+    imports.wbg.__wbindgen_closure_wrapper8294 = function() { return logError(function (arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 747, __wbg_adapter_46);
         return addHeapObject(ret);
     }, arguments) };
 
